50689df240b35f980f9ea2ab608d66afd1e5611c
screamingColor <1121099779@qq.com>
2016-04-01 22:27:49 +0800

    事件触发的次数，流中断重启的过程

--- node/stream_event.js
+++ node/stream_event.js
@@ -1,16 +1,25 @@
 var fs = require('fs')
 var readStream = fs.createReadStream('events.js')
+var n=0
 readStream
 .on('data',function(chunk){
+	n++
 	console.log('data emits')
 	console.log(Buffer.isBuffer(chunk))
-	console.log(chunk.toString('utf8'))
+	/*console.log(chunk.toString('utf8'))*/
+	readStream.pause()
+	console.log('data pause')
+	setTimeout(function(){
+		console.log('data pause end')
+		readStream.resume()//重新启动
+	},3000)
 })
 .on('readable',function(){
 	console.log('data readable')
 })
 .on('end',function(){
 	console.log('data end')
+	console.log(n)
 })
 .on('close',function(){
 	console.log('data close')
