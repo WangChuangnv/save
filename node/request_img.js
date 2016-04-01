7ccf3266b3df8537aa07a737eb4b5a2b85da8cf6
screamingColor <1121099779@qq.com>
2016-04-01 23:01:04 +0800

    常规http

---
+++ node/request_img.js
@@ -0,0 +1,18 @@
+var http=require('http')
+var fs=require('fs')
+
+http
+	.createServer(function(req,res){
+		fs.readFile('z.jpg',function(err,data){
+			if(err){
+				res.end('file not exist!')
+			}
+			else{
+				console.log('start')
+				res.writeHeader(200,{'Context-Type':'text/html'})
+				res.end(data)
+			}
+		})
+		/*fs.createReadStream('z.jpg').pipe(res)*/
+	})
+	.listen(8090)
