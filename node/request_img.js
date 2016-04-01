9b8a440c33389301e61f4b29eb527f4f897dd055
screamingColor <1121099779@qq.com>
2016-04-01 23:03:02 +0800

    pipe()

--- node/request_img.js
+++ node/request_img.js
@@ -3,7 +3,7 @@ var fs=require('fs')
 
 http
 	.createServer(function(req,res){
-		fs.readFile('z.jpg',function(err,data){
+		/*fs.readFile('z.jpg',function(err,data){
 			if(err){
 				res.end('file not exist!')
 			}
@@ -12,7 +12,7 @@ http
 				res.writeHeader(200,{'Context-Type':'text/html'})
 				res.end(data)
 			}
-		})
-		/*fs.createReadStream('z.jpg').pipe(res)*/
+		})*/
+		fs.createReadStream('z.jpg').pipe(res) /*管道*/
 	})
 	.listen(8090)
