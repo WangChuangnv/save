c16a090377a7d03bb596eb7544fcbc6d5bf6c58a
screamingColor <1121099779@qq.com>
2016-04-18 19:38:12 +0800

    考试系统页面完善

--- 考试系统/js/main.js
+++ 考试系统/js/main.js
@@ -1,5 +1,5 @@
 window.onload = function(){
-        function getTypeELement(es,type){//获取指定节点
+        function getTypeELement(es,type){
             var esLen = es.length,
                 i = 0,
                 eArr = [],
@@ -12,21 +12,21 @@ window.onload = function(){
             }
             return eArr;
         }
-        function treeMenuNav(){ //树形菜单导航
-            var as = document.getElementsByTagName("a"),//获取所有的a元素
-                ai = 0,                            //循环变量初始引导值
-                al = as.length,                    //a的个数
-                ao = null;//被遍历的当前元素
+        function treeMenuNav(){
+            var as = document.getElementsByTagName("a"),
+                ai = 0,
+                al = as.length,
+                ao = null;
 
             for(;ai<al;ai++){
                 ao = as[ai];
-                if(ao.className == "treelcon"){ //判断是否是树形节点被单击的地方
-                    ao.onclick = function(){//绑定单击事件
-                        var iconType = this.innerHTML,//获取展示类型
+                if(ao.className == "treelcon"){
+                    ao.onclick = function(){
+                        var iconType = this.innerHTML,
                             uls = getTypeELement(this.parentNode.parentNode.childNodes,"ul"),
-                            uli = 0,          //元素初始值
-                            ull = uls.length,//子菜单个数
-                            dis = "block";  //默认显示（展开）子菜单
+                            uli = 0,
+                            ull = uls.length,
+                            dis = "block";
                         if(iconType == "-"){
                             this.innerHTML = "+";
                             dis="none";
@@ -42,6 +42,51 @@ window.onload = function(){
             }
         }
         treeMenuNav();
+
+
+    /*tab*/
+    function Tab(){
+        var lists= document.getElementsByClassName('list_tab');
+
+        console.log(lists);
+        for(var i= 0;i<lists.length;i++){
+            (function(i){
+                var bodys = lists[i].getElementsByClassName('list_tab_body');
+                var tabs = lists[i].getElementsByTagName('li');
+                console.log(tabs);
+               for(var k=0;k<tabs.length;k++){
+                   (function(k){
+
+                    tabs[k].onclick=function(){
+                        var index = parseInt(tabs[k].getAttribute('index'));
+
+                        for(var m=0;m<tabs.length;m++){
+                            if(tabs[m].className=="click_state"){
+                                tabs[m].className='';
+                            }
+                        }
+                        for(var n=0;n<bodys.length;n++){
+                            if(bodys[n].className=="list_tab_body show"){
+                                bodys[n].className='list_tab_body';
+                            }
+                        }
+
+                       tabs[index - 1].className ='click_state';
+                        bodys[index - 1].className ='list_tab_body show';
+                     };
+
+                   }(k))
+               }
+
+            }(i))
+        }
+
+
+    }
+
+
+
+    Tab();
     };
 
 
